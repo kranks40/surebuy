c5b54c88120c3a578109812bd9738b01271e1319
kranks40 <ketoranks29@gmail.com>
2022-11-06 11:46:14 -0400

    removed frontend content to root

---
+++ backend/schemas/product.js
@@ -0,0 +1,14 @@
+export default {
+  name: "product",
+  title: "Product",
+  type: "document",
+
+  fields: [
+    {
+      name: "image",
+      title: "Image",
+      type: "array",
+      of: [{ type: "image" }],
+    },
+  ],
+};
