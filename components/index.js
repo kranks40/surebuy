43b7b4f2cd17c43ac7dabaa27ef21783723e25f0
kranks40 <ketoranks29@gmail.com>
2022-12-28 08:16:42 -0400

    removed login feature

--- components/index.js
+++ components/index.js
@@ -3,6 +3,6 @@ export { default as Footer } from "./Footer";
 export { default as FooterBanner } from "./FooterBanner";
 export { default as HeroBanner } from "./HeroBanner";
 export { default as Layout } from "./Layout";
-export { default as Login } from "./Login";
+// export { default as Login } from "./Login";
 export { default as Navbar } from "./Navbar";
 export { default as Product } from "./Product";
